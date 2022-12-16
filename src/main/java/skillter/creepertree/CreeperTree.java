9b19314271baac13c620da7d9781a4b36c17d178
Skillter <57605954+Skillter@users.noreply.github.com>
2022-12-16 16:49:32 +0100

    Register the listener

--- src/main/java/skillter/creepertree/CreeperTree.java
+++ src/main/java/skillter/creepertree/CreeperTree.java
@@ -1,17 +1,22 @@
 package skillter.creepertree;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import skillter.creepertree.listener.PlayerMinedTree;
 
 public final class CreeperTree extends JavaPlugin {
 
+    public static CreeperTree INSTANCE = null;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        INSTANCE = this;
+        Bukkit.getPluginManager().registerEvents((Listener) new PlayerMinedTree(), this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        INSTANCE = null;
     }
 }
